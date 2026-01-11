72021477c87a07a86a6dade7d9a1706c7ff0937e
IPhone 14 pro special edition for a boy who is not only cute but also hansome <goljeol@gmail.com>
2026-01-12 01:51:21 +0900

    feat: load_data() NeRF/3DGS 학습용 데이터 로더 구현

---
+++ src/data_loader.py
@@ -0,0 +1,33 @@
+import os
+import json
+import torch
+import numpy as np
+from PIL import Image
+import math
+
+def load_data(base_dir):
+    with open(os.path.join(base_dir, f'transforms_train.json'), 'r') as f:
+        meta = json.load(f)
+
+    datas = []
+        
+    for frame in meta['frames']:
+        img_path = os.path.join(base_dir, frame['file_path'] + '.png')
+
+        image = torch.from_numpy(np.array(Image.open(img_path)) / 255.0).float()
+        image = image[:, :, :3] # RGBA -> RGB (4채널 제거)
+
+        c2w = torch.tensor(frame["transform_matrix"]).float()
+        c2w[0:3, 1:3] *= -1 # Blender -> OpenCV
+
+        H, W = image.shape[:2]
+        camera_angle_x = meta['camera_angle_x']
+        focal = .5 * W / math.tan(0.5 * camera_angle_x)
+
+        datas.append({
+            "c2w": c2w,
+            "image": image,
+            "hwf": [H, W, focal]
+        })
+
+    return datas
