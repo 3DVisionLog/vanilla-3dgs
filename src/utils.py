562e19c6bf14bac279f04d48545f071ffe4835e9
DESKTOP-DQN8BQ4\golje <https://github.com/000uk>
2026-01-14 10:33:13 +0900

    feat: render 함수 추가

--- src/utils.py
+++ src/utils.py
@@ -12,3 +12,16 @@ def set_seed(seed: int = 42):
     # 연산 재현성
     torch.backends.cudnn.deterministic = True  # cuDNN 연산을 determinisitc으로 강제
     torch.backends.cudnn.benchmark = False     # CUDA 성능 자동 튜닝 기능 끔 → 완전 재현 가능
+
+def get_conic(cov2d): # 역행렬 계산
+    a = cov2d[:, 0, 0]
+    b = cov2d[:, 0, 1]
+    c = cov2d[:, 1, 1]
+
+    det = a * c - b**2 # (det = ac - b^2)
+
+    inv_xx =  c / det
+    inv_yy =  a / det
+    inv_xy = -b / det
+
+    return torch.stack([inv_xx, inv_yy, inv_xy], dim=1)
