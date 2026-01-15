1367486b3f2a6b52d770676d5bdc1d9b33a70f15
DESKTOP-DQN8BQ4\golje <https://github.com/000uk>
2026-01-16 06:38:45 +0900

    feat: coarse-fine 학습 구조에 맞게 학습 파이프라인 수정

--- src/runner.py
+++ src/runner.py
@@ -2,14 +2,12 @@ from src.nerf.render import volume_render
 from src.nerf.sampling import sample_z_vals
 from src.nerf.encoder import positional_encode
 
-def run_nerf(model, rays_o, rays_d, n_samples, train=False):
+def run_nerf(model, rays_o, rays_d, z_vals):
     # 좌표 정의 // 좌표(pts) = 출발점(o) + 거리(z_vals) x 방향(d)
-    device = rays_o.device
+    n_rays, n_samples = z_vals.shape
 
-    near, far = 2.0, 6.0
-    z_vals = sample_z_vals(near, far, n_samples, rays_o.shape[0], device, train)
     pts = rays_o[..., None, :] + rays_d[..., None, :] * z_vals[..., :, None] # (N_rand, 64, 3)
-    flat_pts = pts.reshape(-1, 3).to(device) # MLP는 (B, D) 일케 2차원만 받으니까 flatten 해줘야함
+    flat_pts = pts.reshape(-1, 3) # MLP는 (B, D) 일케 2차원만 받으니까 flatten 해줘야함
     encoded_pts = positional_encode(flat_pts, L=10) # (n_rays * n_samples, 3)
     
     # dirs_expanded = rays_d[..., None, :].expand(pts.shape)
@@ -17,11 +15,9 @@ def run_nerf(model, rays_o, rays_d, n_samples, train=False):
     # encoded_dirs = positional_encode(flat_dirs, L=4)
     encoded_dirs = positional_encode(rays_d, L=4)   # (n_rays, D)
     encoded_dirs = encoded_dirs[:, None, :].expand(-1, n_samples, -1)
-    encoded_dirs = encoded_dirs.reshape(-1, encoded_dirs.shape[-1]).to(device)
+    encoded_dirs = encoded_dirs.reshape(-1, encoded_dirs.shape[-1])
 
     raw = model(encoded_pts, encoded_dirs) # (n_rays * n_samples, 4)
     raw = raw.reshape(rays_o.shape[0], n_samples, 4) # (n_rays, n_samples, 4)
 
-    rgb = volume_render(raw, z_vals)
-
-    return rgb
+    return raw
